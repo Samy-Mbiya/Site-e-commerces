30a9d000cde6ee344efd5843156c89034d09ef24
Samy-Mbiya <samymbiya@gmail.com>
2021-12-22 16:10:23 +0100

    Affichage d'ajout de l'element  du pagier

--- AliExprass/src/Controller/CartController.php
+++ AliExprass/src/Controller/CartController.php
@@ -10,43 +10,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $cartServices;
+    public function __construct(CartServices $cartServices)
+    {
+        $this->cartServices = $cartServices;
+    }
+
     /**
      * @Route("/cart", name="cart")
      */
-    public function index(CartServices $cartServices): Response //Extention de l'objet $cartServices qui est dans la class CartServices 
+    public function index(): Response //Extention de l'objet $cartServices qui est dans la class CartServices 
     {
-        $cartServices->addtoCart(3); // Recuperation de la methode addtoCart
-        dd($cartServices->getCart()); // Recuperation de la methode getCart
+        //$cartServices->addtoCart(3); // Recuperation de la methode addtoCart
+        //dd($cartServices->getCart()); // Recuperation de la methode getCart
+        $cart = $this->cartServices->getfullCart();
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
+            'cart' => $cart,
         ]);
     }
 
     /**
-     * @Route("/cart/add/{id}")
+     * @Route("/cart/add/{id}" , name="cart_add")
      */
 
     public function addtoCart($id, CartServices $cartServices): Response // Ajout de Produit dans le pagnier
     {
-        //$cartServices->deleteCart();
-        $cartServices->addtoCart($id);
-        dd($cartServices->getfullCart()); // Recuperation de la methode getCart
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        $this->cartServices->addtoCart($id);
+        return $this->redirectToRoute("cart"); // on redirige vers le la route cart
     }
 
     /**
-     * @Route("/cart/delete/{id}")
+     * @Route("/cart/delete/{id}", name="cart_delete")
      */
 
     public function deleteFromCart($id, CartServices $cartServices): Response // Suppression d'une commande du produit dans le panier
     {
         //$cartServices->deleteCart();
-        $cartServices->deleteFromCart($id);
-        dd($cartServices->getfullCart()); // Recuperation de la methode getCart
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
+        $this->cartServices->deleteFromCart($id);
+        return $this->redirectToRoute("cart"); // on redirige vers le la route cart
     }
 }
